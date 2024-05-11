beb415db6a024ba4a9b5fb9f0e46e6941eb5b811
zacksatylgan <166334246+zacksatylgan@users.noreply.github.com>
2024-05-11 15:44:36 -0500

    first commit

---
+++ src/main/java/org/example/MyPersonalClass.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class MyPersonalClass {
+
+    public static void main(String[] args) {
+
+        System.out.println( "Hello folks! This is new project" );
+
+    }
+
+}
